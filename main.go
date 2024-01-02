69512ad6db52da4b1627265dc7cb1078dd791776
Alexander F. Rødseth <rodseth@gmail.com>
2024-01-02 16:06:59 +0100

    Minor changes

--- main.go
+++ main.go
@@ -22,16 +22,6 @@ func angleLength(input uint8) (uint8, uint8) {
 	return angleByte, length
 }
 
-func drawCircle(img draw.Image, center image.Point, radius int, col color.Color) {
-	for dY := -radius; dY <= radius; dY++ {
-		for dX := -radius; dX <= radius; dX++ {
-			if dX*dX+dY*dY <= radius*radius {
-				img.Set(center.X+dX, center.Y+dY, col)
-			}
-		}
-	}
-}
-
 func drawLine(img draw.Image, start, end image.Point, col color.Color) {
 	dx := math.Abs(float64(end.X - start.X))
 	dy := math.Abs(float64(end.Y - start.Y))
@@ -44,14 +34,11 @@ func drawLine(img draw.Image, start, end image.Point, col color.Color) {
 		sy = 1
 	}
 	err := dx - dy
-
 	for {
 		img.Set(start.X, start.Y, col)
-
 		if start.X == end.X && start.Y == end.Y {
 			break
 		}
-
 		e2 := 2 * err
 		if e2 > -dy {
 			err -= dy
@@ -60,6 +47,16 @@ func drawLine(img draw.Image, start, end image.Point, col color.Color) {
 		if e2 < dx {
 			err += dx
 			start.Y += sy
+		}
+	}
+}
+
+func drawCircle(img draw.Image, center image.Point, radius int, col color.Color) {
+	for dY := -radius; dY <= radius; dY++ {
+		for dX := -radius; dX <= radius; dX++ {
+			if dX*dX+dY*dY <= radius*radius {
+				img.Set(center.X+dX, center.Y+dY, col)
+			}
 		}
 	}
 }
@@ -82,31 +79,35 @@ func drawVector(angle, length uint8) *image.RGBA {
 	endY := center.Y + int(float64(length)*math.Sin(float64(angle)*(2*math.Pi/255)))
 
 	// Draw vector line
-	drawLine(img, center, image.Point{endX, endY}, color.RGBA{255, 0, 0, 255}) // Red line
+	drawLine(img, center, image.Point{endX, endY}, color.RGBA{255, 0, 0, 255})
 
 	return img
 }
 
-func saveImage(img *image.RGBA, filename string) {
+func saveImage(img *image.RGBA, filename string) error {
 	f, err := os.Create(filename)
 	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
+		return err
 	}
 	defer f.Close()
-
-	if err := png.Encode(f, img); err != nil {
-		fmt.Println("Error encoding PNG:", err)
-	}
+	return png.Encode(f, img)
 }
 
 func main() {
+	var (
+		filename string
+		angle    byte
+		length   byte
+		img      *image.RGBA
+	)
 	for i := 0; i <= 255; i++ {
-		angle, length := angleLength(uint8(i))
-		fmt.Printf("![input %d](img/vector%03d.png)\n", i, i)
+		filename = fmt.Sprintf("img/vector%03d.png", i)
+		angle, length = angleLength(uint8(i))
+		fmt.Printf("![input %d](%s)\n\n", i, filename)
 		fmt.Printf("    input %d -> angle %f, length %f\n\n", i, float64(angle)/255.0, float64(length)/255.0)
-		img := drawVector(angle, length)
-		filename := fmt.Sprintf("img/vector%03d.png", i)
-		saveImage(img, filename)
+		img = drawVector(angle, length)
+		if err := saveImage(img, filename); err != nil {
+			fmt.Printf("error saving image: %v\n", err)
+		}
 	}
 }
